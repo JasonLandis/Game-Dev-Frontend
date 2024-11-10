23760786693af53edfaebc4fbe0dab4d475b3ab3
Jason Landis <jasonelandis@gmail.com>
2024-11-09 23:48:42 -0500

    made games page

---
+++ src/app/games/page.tsx
@@ -0,0 +1,28 @@
+export default async function GamesPage() {
+
+  const data = await fetch('http://localhost:4000/api/games');
+  const value = await data.json();
+
+  interface gameObject {
+    game_id: number,
+    developer: number,
+    name: string,
+    description: string,
+    release_date?: Date,
+    price?: number
+  }
+
+  return (    
+    <main>
+      <h1 className="font-bold mb-5">Game List</h1>
+      <ul>
+        {value.map((game: gameObject) => (
+          <li key={game.game_id}>
+            <h2 className="font-medium">{game.name}</h2>
+            <p>Description: {game.description}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
